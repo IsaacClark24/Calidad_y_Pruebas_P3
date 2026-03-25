04468c4899fa34c5f407a592b78456e89b139aea
Paulina Mendez <paulina.mendez@cua.uam.mx>
2026-03-24 19:42:12 -0600

    Se agregaron los ejercicios 1, 2 y 3.

--- src/main/java/org/isaac/mucino/model/LibroModel.java
+++ src/main/java/org/isaac/mucino/model/LibroModel.java
@@ -12,6 +12,12 @@ public record LibroModel(
         @Size(max = 60, message = "El Autor No Puede Superar Los 60 Caracteres")
         @Pattern(regexp = "^[\\p{L} \\-.&,]+$", message = "El Nombre Del Autor(es) No Puede Llevar Caracteres No Validos")
         String autor,
+        @Min(value = 1, message = "La edición mínima es 1")
+        @Max(value = 20, message = "La edición máxima es 20")
+        int edicion,
+        @NotBlank(message = "La editorial no puede estar vacía")
+        @Pattern(regexp = "^[\\p{L}\\s.&]+$", message = "La editorial solo acepta letras, espacios, puntos y '&'")
+        String editorial,
         @Min(value = 1899, message = "El Año No Puede Ser Menor A 1990")
         @Max(value = 2027, message = "El Año No Puede Ser Mayor A 2026")
         int anio
@@ -19,5 +25,6 @@ public record LibroModel(
     public LibroModel {
         titulo = titulo == null ? null : titulo.trim();
         autor = autor == null ? null : autor.trim();
+        editorial = (editorial == null) ? null : editorial.trim();
     }
 }
